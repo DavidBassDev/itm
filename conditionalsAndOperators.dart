b3ea74a8af035c573ecaf906c4f0823ef1153e7e
unknown <andres.a97@hotmail.com>
2024-09-29 10:07:51 -0500

    primer ejercicio resuelto

--- conditionalsAndOperators.dart
+++ conditionalsAndOperators.dart
@@ -1,13 +1,23 @@
 import 'dart:io';
 
 void main() {
-  print("escibe un numero");
-  double? numero = double.tryParse(stdin.readLineSync()!);
-  print("escibe segundo numero");
-  double? numero2 = double.tryParse(stdin.readLineSync()!);
-  if (numero != null && numero2 != null) {
-    print("suma es ${numero + numero2}");
-  } else {
-    print("alguno de los dos numeros no son validos");
+  var problema1 = new positivePower();
+  double resultado1 = problema1.positivepower();
+  print("el resultado del primer ejercicio es $resultado1");
+}
+
+class positivePower {
+  double positivepower() {
+    double resultado = 0.0;
+    print("escibe un numero");
+    double? numero = double.tryParse(stdin.readLineSync()!);
+    print("escibe segundo numero");
+    double? numero2 = double.tryParse(stdin.readLineSync()!);
+    if (numero != null && numero2 != null) {
+      resultado = numero + numero2;
+    } else {
+      print("alguno de los dos numeros no son validos");
+    }
+    return resultado;
   }
 }
