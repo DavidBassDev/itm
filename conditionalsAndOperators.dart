f08d280bacd2f3f18badf0b1607c2f3b44bb56e6
unknown <andres.a97@hotmail.com>
2024-09-30 13:10:31 -0500

    segundo ejercicio resuelto

--- conditionalsAndOperators.dart
+++ conditionalsAndOperators.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
 void main() {
-  var problema1 = new positivePower();
+  /* var problema1 = new positivePower();
   String resultado1 = problema1.positivepower();
   print("el resultado del primer ejercicio es $resultado1");
+*/
+  //EJERCICIO 2
+  var problema2 = new DoubleOrTriple();
+  int resultado2 = problema2.doubleortriple();
+  print("el resultado del ejercicio 2 es $resultado2");
 }
 
 class positivePower {
@@ -21,8 +26,53 @@ class positivePower {
         resultado = "0";
       }
     } else {
-      print("alguno de los dos numeros no son validos");
+      print("no es numero ");
     }
     return resultado;
   }
+}
+
+class DoubleOrTriple {
+  int doubleortriple() {
+    int resultado = 0;
+    bool numero = false;
+    print("primer numero");
+    String? entrada = stdin.readLineSync();
+    numero = esNumero(entrada);
+    while (!numero) {
+      print("primer numero");
+      String? entrada = stdin.readLineSync();
+      numero = esNumero(entrada);
+    }
+    int? numero1 = int.tryParse(entrada!);
+    numero = false;
+    print("segundo numero");
+    String? entrada2 = stdin.readLineSync();
+    numero = esNumero(entrada2);
+    while (!numero) {
+      print("segundo numero");
+      String? entrada2 = stdin.readLineSync();
+      numero = esNumero(entrada);
+    }
+    int? numero2 = int.tryParse(entrada2!);
+
+    if (numero1 != null && numero2 != null) {
+      if (numero1 > numero2) {
+        resultado = numero1 + numero1;
+      } else if (numero2 > numero1) {
+        resultado = numero2 * 3;
+      } else {
+        resultado = numero1 + numero1;
+      }
+    }
+    return resultado;
+  }
+}
+
+bool esNumero(String? input) {
+  if (input == null) {
+    return false;
+  }
+
+  return int.tryParse(input) != null;
 }
